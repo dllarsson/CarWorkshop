598c05be8e6b4720329320103946fe5ee9181992
David Larsson <david.l.larsson.90>
2019-10-26 14:43:37 +0200

    Added classes

---
+++ CarWorkshopConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarWorkshopConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassLibrary/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace ClassLibrary
+{
+    public abstract class Vehicle
+    {
+        public string Manufacturer { get; private set; }
+        public int ModelYear { get; private set; }
+        public string LicensePlate { get; private set; }
+    }
+}
